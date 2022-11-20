2a7b8ccd709f7e68be0f59eebe8183dbcffbe104
Murilo Weishaupt de Camargo <muriloweishaupt@gmail.com>
2022-11-20 18:52:36 -0200

    Estrutura de repetição "For"

---
+++ aula14/ambiente.js
@@ -0,0 +1,16 @@
+// Estrutura de repetição com Teste no começo
+var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+// Estrutura de repetição com Teste no Final
+do {
+    console.log (`Passo ${c}`)
+} while (c <= 10)
+
+// Esrutura de repetição com Teste e complemento
+for (var c = 1; c <= 10; c++) {
+    console.log (`Passo ${c}`)
+}
