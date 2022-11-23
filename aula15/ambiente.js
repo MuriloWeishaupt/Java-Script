3ef8fc54131ebb0c312f58019baf3511c45dba6a
Murilo Weishaupt de Camargo <muriloweishaupt@gmail.com>
2022-11-23 17:46:55 -0200

    Arrays (Vetores)

---
+++ aula15/ambiente.js
@@ -0,0 +1,14 @@
+var valores = [1, 3, 2,]
+valores.push(4)
+valores.push(8)
+valores.push(7)
+valores.sort()
+console.log(`O Vetor tem ${valores.length} posições`)
+console.log(`O valor que está na primeira posição é o número ${valores.indexOf(3)}`)
+
+console.log(`${valores}`)
+
+
+
+
+
---
+++ aula15/teste.js
@@ -0,0 +1,17 @@
+var valores = [1, 5, 7]
+/*
+console.log(`${valores}`)
+
+for(var pos = 0; pos<= valores.length;pos++) {
+    console.log(valores[pos])
+}
+
+
+for(var pos in valores) {
+    console.log (valores[pos])
+}
+*/
+
+for(var pos = 0; pos <= valores.length;pos++) {
+    console.log(`${pos}`)
+}
