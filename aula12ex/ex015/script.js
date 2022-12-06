95037d715219722ed441ad9e565f6259e53034dc
Murilo Weishaupt de Camargo <muriloweishaupt@gmail.com>
2022-12-06 12:51:19 -0200

    Arrumei os códigos

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -3,17 +3,14 @@ var foto = window.document.getElementById ('imagem')
 var data = new Date()
 var hora = data.getHours()
 
-var hora = 
-
-
 msg.innerHTML = `Agora são ${hora} horas.`
     if (hora > 0 && hora < 12) {
-        Img.src = 'imagem-manhã.jpg'
-        document.body.style.background = '#e2cd9f'
+        imagem.src = 'imagemmanha.jpg'
+        document.body.style.background = '#f3ed5f'
     } else if (hora >= 12 && hora < 19){
-        fotos.src = 'imagem-tarde.jpg'
+        imagem.src = 'imagemtarde.jpg'
         document.body.style.background = '#b9846f'
     } else {
-        fotos.src = 'imagem-noite.jpg'
+        imagem.src = 'imagemnoite.jpg'
         document.body.style.background = '#515154'
     }
---
+++ aula16ex/modelo - Copia/script.js
@@ -0,0 +1,8 @@
+var num = window.document.getElementById('fnum')
+var lista = window.document.getElementById('flista')
+var res = window.document.getElementById('res')
+var valores = []
+
+function adicionar() {
+    if( Number(n) >= 1 && Number(n) <= 100 && )
+}
