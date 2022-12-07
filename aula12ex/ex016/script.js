a8fd68a8d322f2788e89421a11aeac477fd2497f
Murilo Weishaupt de Camargo <muriloweishaupt@gmail.com>
2022-12-07 17:13:37 -0200

    ex03

--- aula12ex/ex016/script.js
+++ aula12ex/ex016/script.js
@@ -1,51 +1,51 @@
 function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fAno = window.document.getElementById('txtano')
-    var res = window.document.getElementById('res')
-    if (fAno.value.lenght == 0 || fAno.value > ano) {
-        window.alert ('[ERRO] verifique os dados e tente novamente!')
-    } else {
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano = Number(fAno.value)
-       var gênero = ''
-       var img = document.createElement('img')
-       img.setAttribute('id', 'foto')
-        if (fsex[0].checked) {
-            gênero = 'Homem'
-             if (idade < 10) {
-                // Criança
-                img.setAttribute('src', 'bebe.jpg')
-             } else if (idade < 21) {
-                // Jovem
-                img.setAttribute('src', 'jovem-m.jpg')
-             } else if (idade < 50) {
-                // Adulto
-                img.setAttribute('src', 'homem-adulto.jpg')
-             } else {
-                // Idoso
-                img.setAttribute('homem-idoso.jpg')
-             }
-        } else {
-            gênero = 'Mulher'
-            if (idade < 10) {
-                // Criança
-                img.setAttribute('src', 'bebe.jpg')
-             } else if (idade < 21) {
-                // Jovem
-                img.setAttribute('src', 'jovem-f.jpg')
-
-             } else if (idade < 50) {
-                // Adulto
-                img.setAttribute ('mulher.adulta.jpg')
-             } else {
-                // Idoso
-                img.setAttribute('src', 'mulher-idosa.jpg')
-             }
-        }
-        res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
-       res.appendChild(img)
-        
-    } 
+  var data = new Date()
+  var ano = data.getFullYear()
+  var fano = window.document.getElementById('txtano')
+  var res = window.document.getElementById('res')
+      if(fano.value.length == 0 || Number(fano.value) > ano ) {
+          window.alert('[ERRO]')
+      } else {
+          var fsex = window.document.getElementsByName('radsex')
+          var idade = ano - Number(fano.value)
+         var genero = ''
+         var img = document.createElement('img')
+         img.setAttribute('id', 'foto')
+          if(fsex[0].checked) {
+              genero = 'Homem'
+               if(idade >= 0 && idade <= 10) {
+                  // Criança
+                  img.setAttribute('src', 'bebe.jpg')
+               }
+               else if (idade <= 21) {
+                  // Adolescente
+                  img.setAttribute('src', 'jovem-m.jpg')
+               } else if (idade < 50) {
+                  // Adulto
+                  img.setAttribute('src', 'homem-adulto.jpg')
+               } else {
+                  // Idoso
+                  img.setAttribute('src', 'homem-idoso.jpg')
+               }
+          } else {
+              genero = 'Mulher'
+              if(idade >= 0 && idade <= 10) {
+                  // Criança
+                  img.setAttribute('src', 'bebe.jpg')
+               }
+               else if (idade <= 21) {
+                  // Adolescente
+                  img.setAttribute('src', 'jovem-f.jpg')
+               } else if (idade < 50) {
+                  // Adulto
+                  img.setAttribute('src', 'mulher-adulta.jpg')
+               } else {
+                  // Idoso
+                  img.setAttribute('src', 'mulher-idosa.jpg')
+               }
+          }
+          res.innerHTML  = `<p>Detectamos ${genero} com ${idade} anos</p>`
+          res.appendChild(img)
+          
+      }
 }
